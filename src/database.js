221799bf9d84097ac87de09cb1897b6931957a46
Josue <josueisaiasmartinezlopez@gmail.com>
2023-08-23 15:55:48 -0600

    Mejorando login.js

--- src/database.js
+++ src/database.js
@@ -9,14 +9,16 @@ const mysql = require ('mysql2');
   database: 'AXE',
   multipleStatements: true
 });*/
-/*const mysqlConnection = mysql.createConnection({
+
+const mysqlConnection = mysql.createConnection({
   host: '142.44.161.115',
   port: 3306,
   user: 'ALIEDU',
   password: 'ALIEDU##23aa',
   database: 'ALIEDU',
   multipleStatements: true
-});*/
+});
+/*
 const mysqlConnection = mysql.createConnection({
   host: '82.180.162.18',
   port: 3306,
@@ -25,7 +27,7 @@ const mysqlConnection = mysql.createConnection({
   database: 'axe',
   multipleStatements: true
 });
-
+*/
 //Esto es para trabajar con mi BD de manera local, quitar, cuando se conecta al servidor remoto
 
 mysqlConnection.connect(function (err) {
