98e70aa63a509458e7bb16ffcab0496b13f67eae
EMerazzz <erlyn.meraz@unah.hn>
2023-08-23 20:55:55 -0600

    Conexion nube

--- src/database.js
+++ src/database.js
@@ -10,15 +10,15 @@ const mysql = require ('mysql2');
   multipleStatements: true
 });*/
 
-const mysqlConnection = mysql.createConnection({
+/*const mysqlConnection = mysql.createConnection({
   host: '142.44.161.115',
   port: 3306,
   user: 'ALIEDU',
   password: 'ALIEDU##23aa',
   database: 'ALIEDU',
   multipleStatements: true
-});
-/*
+});*/
+
 const mysqlConnection = mysql.createConnection({
   host: '82.180.162.18',
   port: 3306,
@@ -27,7 +27,7 @@ const mysqlConnection = mysql.createConnection({
   database: 'axe',
   multipleStatements: true
 });
-*/
+
 //Esto es para trabajar con mi BD de manera local, quitar, cuando se conecta al servidor remoto
 
 mysqlConnection.connect(function (err) {
