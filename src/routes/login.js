24363ab4c352aa0e3b6edc9b1fda94c80dba79fe
Josue <josueisaiasmartinezlopez@gmail.com>
2023-08-26 08:59:48 -0600

    Agregando try-catch a login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -10,69 +10,85 @@ global.secretTokenAccess = 'my_token_for_access'
 
 // ============
 router.post('/login', (req, res) => {
-    const { USUARIO, CONTRASENA } = req.body;
+    try {
+      const { USUARIO, CONTRASENA } = req.body;
   
-     mysqlConnection.query("CALL MS_Autenticacion(?, ?)",  [USUARIO, CONTRASENA], (error, results) => {
-      if (error) {
-        res.status(500).json({ error: 'Error interno del servidor' });
-      } else {
-        respuestaBD = results[0][0];
-        const cantidadPropiedades = Object.keys(respuestaBD).length;
-
-        if (cantidadPropiedades > 1) {
-          const token = jwt.sign({ USUARIO, CONTRASENA }, secretTokenAccess, {expiresIn:'1h'}, (err, token) => {
-            console.log("Respuesta OKK")
-            //Generación de token 
-            res.json({
-              token,
-              USUARIO
-            });
-          });
-          //La anterior instrucción muestra el token del usuario para poder usar las APIs.
+      mysqlConnection.query("CALL MS_Autenticacion(?, ?)", [USUARIO, CONTRASENA], (error, results) => {
+        if (error) {
+          res.status(500).json({ error: 'Error interno del servidor' });
         } else {
-          res.status(400).json(respuestaBD);
+          respuestaBD = results[0][0];
+          const cantidadPropiedades = Object.keys(respuestaBD).length;
+  
+          if (cantidadPropiedades > 1) {
+            const token = jwt.sign({ USUARIO, CONTRASENA }, secretTokenAccess, { expiresIn: '1h' }, (err, token) => {
+              if (err) {
+                throw err; // Throw error to be caught by the outer try-catch
+              }
+              console.log("Respuesta OKK");
+              //Generación de token 
+              res.json({
+                token,
+                USUARIO
+              });
+            });
+            //La anterior instrucción muestra el token del usuario para poder usar las APIs.
+          } else {
+            res.status(400).json(respuestaBD);
+          }
         }
-      }
-    });
+      });
+    } catch (catchError) {
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
   });
-
+  
 
   // ============ 
-router.post('/usuario_contrasena', (req, res) => {
-  const { USUARIO} = req.body;
-
-   mysqlConnection.query("CALL MS_buscarUsuario(?)",  [USUARIO], (error, results) => {
-    if (error) {
-      res.status(500).json({ error: 'Error interno del servidor' });
-    } else {
-      respuestaBD = results[0][0];
-      const cantidadPropiedades = Object.keys(respuestaBD).length;
-
-      if (cantidadPropiedades > 1) {
-        res.status(200).json(results[0]);
-        //La anterior instrucción muestra el token del usuario para poder usar las APIs.
-      } else {
-        res.status(400).json(respuestaBD);
-      }
-    }
-  });
-});
-
-router.post('/cambiarContrasena', (req, res) => {
-  const { USUARIO, CONTRASENA} = req.body;
+  router.post('/usuario_contrasena', (req, res) => {
+    try {
+      const { USUARIO } = req.body;
   
-   mysqlConnection.query("CALL MS_cambiarContrasena(?,?)",  [USUARIO, CONTRASENA], (error, results) => {
-    if (error) {
+      mysqlConnection.query("CALL MS_buscarUsuario(?)", [USUARIO], (error, results) => {
+        if (error) {
+          res.status(500).json({ error: 'Error interno del servidor' });
+        } else {
+          respuestaBD = results[0][0];
+          const cantidadPropiedades = Object.keys(respuestaBD).length;
+  
+          if (cantidadPropiedades > 1) {
+            res.status(200).json(results[0]);
+            //La anterior instrucción muestra el token del usuario para poder usar las APIs.
+          } else {
+            res.status(400).json(respuestaBD);
+          }
+        }
+      });
+    } catch (catchError) {
       res.status(500).json({ error: 'Error interno del servidor' });
-    } else {
-
-      //Capturamos el mensaje que nos responde la BD
-      respuestaBD = results[0][0];
-
-      //Respondemos el mensaje que nos responde la BD
-      res.status(200).json(respuestaBD);
     }
   });
-});
+  
+
+  router.post('/cambiarContrasena', (req, res) => {
+    try {
+      const { USUARIO, CONTRASENA } = req.body;
+  
+      mysqlConnection.query("CALL MS_cambiarContrasena(?,?)", [USUARIO, CONTRASENA], (error, results) => {
+        if (error) {
+          res.status(500).json({ error: 'Error interno del servidor' });
+        } else {
+          //Capturamos el mensaje que nos responde la BD
+          respuestaBD = results[0][0];
+  
+          //Respondemos el mensaje que nos responde la BD
+          res.status(200).json(respuestaBD);
+        }
+      });
+    } catch (catchError) {
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+  
 
   module.exports = router;
