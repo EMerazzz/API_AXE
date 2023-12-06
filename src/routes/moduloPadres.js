e84c1b342a14b220b99b2d2c35871a94a0f74bc2
EMerazzz <erlyn.meraz@unah.hn>
2023-12-05 23:38:26 -0600

    padres

--- src/routes/moduloPadres.js
+++ src/routes/moduloPadres.js
@@ -176,7 +176,7 @@ router.put("/padres_tutores/:COD_PADRE_TUTOR", /*verifyToken, */(req, res) => {
         Estado_registro
       } = req.body;
   
-        const sql = `call axe.SP_moduloEstudiantes('ME_PADRES_TUTORES', 'UP', '${COD_PADRE_TUTOR}', '${COD_PERSONA}', '${NOMBRE_PADRE_TUTOR}', '${APELLIDO_PADRE_TUTOR}', '${OCUPACION_PADRE_TUTOR}', '${RELACION_PADRE_ESTUDIANTE}','${USUARIO_MODIFICADOR}')`;
+        const sql = `call axe.SP_moduloEstudiantes('ME_PADRES_TUTORES', 'UP', '${COD_PADRE_TUTOR}', '1', '${NOMBRE_PADRE_TUTOR}', '${APELLIDO_PADRE_TUTOR}', '${OCUPACION_PADRE_TUTOR}', '${RELACION_PADRE_ESTUDIANTE}','${USUARIO_MODIFICADOR}')`;
       mysqlConnection.query(sql, (error) => {
         if (!error) {
           res.json({
